506cee3a7158eac90abf1da86dcd0cbc52a52b97
heflaruz <henryplasenciadelacruz@gmail.com>
2023-02-03 18:35:38 +0100

    animales

---
+++ animales/listado.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>listado de animales</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>LISTADO</h1>
+</div>
+
+<div class="topnav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+
+<div class="row">
+  <div class="column side">
+  </div>
+  
+  <div class="column middle">
+    <!--<div class="parriva"></div>-->
+    <table>
+        <tr>
+            <th>
+                Listado de Peludines
+
+            </th>
+        </tr>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <a href="#">Link</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <a href="#">Crear nuevo animal</a>
+            </td>
+        </tr>
+    </table>
+  </div>
+
+  
+  <div class="column side">
+  </div>
+</div>
+<script src="script.js"></script>
+<div class="footer">
+    <?php
+    echo "Henry Fabricio Plasencia De La Cuz  ". date("d/m/Y") ;
+    ?>
+
+</div>
+  
+</body>
+</html>
